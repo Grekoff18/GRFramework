9b4a2a9a5f263b6a021dda6eddbd884c51e01181
Nick Grekoff <kolagrekalo@gmail.com>
2022-11-21 15:32:51 +0200

    fix: commit cli code

--- src/cli.ts
+++ src/cli.ts
@@ -1,21 +1,21 @@
 #!/usr/bin/env node
-import { GRInstance, GRTree, GRBuilder } from "./index";
+// import { GRInstance, GRTree, GRBuilder } from "./index";
 
-new GRInstance({
-  propData: {
-    parent: "1",
-  },
-  children: [],
-  tag: "p",
-});
-const component = new GRInstance({
-  propData: {
-    parent: "2",
-  },
-  children: [],
-  tag: "div",
-});
+// new GRInstance({
+//   propData: {
+//     parent: "1",
+//   },
+//   children: [],
+//   tag: "p",
+// });
+// const component = new GRInstance({
+//   propData: {
+//     parent: "2",
+//   },
+//   children: [],
+//   tag: "div",
+// });
 
-new GRBuilder(".app", component);
+// new GRBuilder(".app", component);
 
-console.log("grtree", GRTree.treeStore);
+// console.log("grtree", GRTree.treeStore);
